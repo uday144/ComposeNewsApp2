b408afa8071661a8d92fc80ac105ade42800a52e
uday singh <>
2024-06-06 17:07:36 +1000

    Added splash screen

--- app/src/main/java/com/example/mvvmcomposeapp2/MainActivity.kt
+++ app/src/main/java/com/example/mvvmcomposeapp2/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mvvmcomposeapp2.ui.theme.MVVMComposeApp2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             MVVMComposeApp2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
